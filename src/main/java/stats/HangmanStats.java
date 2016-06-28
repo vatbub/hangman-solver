27cf9cc04176db39a9a55fdd776b1b4e4d73c5c7
Frederik Kammel <frederikkammel@outlook.de>
2016-06-29 00:40:20 +0200

    [#2] Offline cache for unsubmitted words is now persistent, uploads can be finished after the next app start

--- src/main/java/stats/HangmanStats.java
+++ src/main/java/stats/HangmanStats.java
@@ -8,6 +8,7 @@ import com.mongodb.client.*;
 import com.mongodb.client.model.Filters;
 import com.mongodb.client.model.Updates;
 
+import common.Prefs;
 import languages.Language;
 
 /**
@@ -21,8 +22,9 @@ public class HangmanStats {
 
 	private static List<String> alreadySubmittedWordsInThisSession = new ArrayList<String>();
 	private static LinkedBlockingQueue<Document> docQueue = new LinkedBlockingQueue<Document>();
+	private static Prefs preferences = new Prefs(HangmanStats.class.getName());
+	private static String persistentDocQueueKey = "docQueue";
 
-	
 	public static Thread uploadThread = new Thread() {
 		private boolean interrupted = false;
 
@@ -30,6 +32,9 @@ public class HangmanStats {
 		public void run() {
 			this.setName("uploadThread");
 			System.out.println("Starting " + this.getName() + "...");
+			
+			readDocQueueFromPreferences();
+			
 			while (!interrupted) {
 				if (MongoSetup.isReachable()) {
 					if (!docQueue.isEmpty()) {
@@ -57,9 +62,54 @@ public class HangmanStats {
 		@Override
 		public void interrupt() {
 			interrupted = true;
+			saveDocQueueToPreferences();
 			System.out.println("Shutting " + this.getName() + " down...");
 		}
 	};
+
+	/**
+	 * Takes the content of the docQueue and saves its content using the Prefs
+	 * class from the common project
+	 */
+	private static void saveDocQueueToPreferences() {
+		System.out.println("Saving docQueue to disk...");
+		String res = "";
+		while (!docQueue.isEmpty()) {
+			Document doc = docQueue.remove();
+			res = res + doc.toJson();
+			if (!docQueue.isEmpty()) {
+				// Still other objects left so add a line break
+				res = res + "\n";
+			}
+		}
+
+		preferences.setPreference(persistentDocQueueKey, res);
+	}
+
+	/**
+	 * Reads the persistent copy of the docQueue if one exists and merges it
+	 * with the docQueue in memory.
+	 */
+	private static void readDocQueueFromPreferences(){
+		System.out.println("Reading docQueue from disk...");
+		
+		String persStr = preferences.getPreference(persistentDocQueueKey, "");
+		
+		String[] docs = persStr.split("\n");
+		
+		for (String newDoc:docs){
+			Document doc = Document.parse(newDoc);
+			if (!docQueue.contains(doc)){
+				try {
+					docQueue.put(doc);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
+		
+	}
 
 	/**
 	 * Adds a word to the mongodb database
@@ -70,8 +120,8 @@ public class HangmanStats {
 	 *            The Language the user is currently playing in
 	 */
 	public static void addWordToDatabase(String word, Language lang) {
-		
-		if (uploadThread.isAlive()==false){
+
+		if (uploadThread.isAlive() == false) {
 			uploadThread.start();
 		}
 
@@ -88,7 +138,6 @@ public class HangmanStats {
 			}
 			System.out.println("Submission done.");
 		}
-
 
 	}
 }
